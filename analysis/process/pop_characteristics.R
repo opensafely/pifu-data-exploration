5e9868bdf1115b7a66a07b9c823f17e53e9d34bd
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2025-05-29 16:10:07 +0000

    Fix table

--- analysis/process/pop_characteristics.R
+++ analysis/process/pop_characteristics.R
@@ -1,5 +1,4 @@
 
-
 # Import libraries #
 library('tidyverse')
 library('fs')
@@ -10,23 +9,23 @@ library('janitor')
 # Create directory
 dir_create(here::here("output", "dataset"), showWarnings = FALSE, recurse = TRUE)
 
-dataset <- read_csv(here::here("output", "dataset.csv.gz")) %>%
-  mutate(first_pfu_year = as.character(first_pfu_year))
+dataset <- read_csv(here::here("output", "dataset.csv.gz")) 
 
+# Create frequency distribution
 freq <- function(var, name) {
   dataset %>%
     mutate(total = n(), variable = name) %>%
-      group_by(variable, category = {{var}}, total) %>%
-      summarise(count = n()) 
-      
+    group_by(variable, category = as.character({{var}}), total) %>%
+    summarise(count = n()) 
+  
 }
 
 table <- rbind(
-    freq(age_group, "age"),
-    freq(sex, "sex"),
-    freq(region, "region"),
-    freq(first_pfu_year, "first PFU year"),
-    freq(treatment_function_code, "treatment function code")
+  freq(age_group, "age"),
+  freq(sex, "sex"),
+  freq(region, "region"),
+  freq(first_pfu_year, "first PFU year"),
+  freq(treatment_function_code, "treatment function code")
 )
 
 write.csv(table, file = here::here("output", "dataset", "table.csv"), row.names = FALSE)
