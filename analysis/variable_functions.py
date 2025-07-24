9e20cfc8b2a6ebca6924d8d3d36b141ca4073712
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2025-07-24 11:50:25 +0000

    Add regional breakdown

--- analysis/variable_functions.py
+++ analysis/variable_functions.py
@@ -26,6 +26,31 @@ def opa_characteristics(all_opa, first_opa, first_pfu):
     dataset.treatment_function_code = first_opa.treatment_function_code # specialty
     dataset.pfu_treatment_function_code = first_pfu.treatment_function_code
 
+
+    ###################################
+    
+    # By treatment specialty (only include most common groups reported in public statistics)
+    trt_func = ["100","101","110","120","130","140","150","160","170","300","301","320","330","340","400","410","430","502"]
+
+    count_var = {}
+
+    for code in trt_func:
+
+        count_var["any_pfu_" + code] = all_opa.where(
+            all_opa.treatment_function_code.is_in([code])
+            & all_opa.outcome_of_attendance.is_in(["4","5"]) 
+            & all_opa.appointment_date.is_on_or_after("2022-06-01")
+        ).exists_for_patient()
+
+        count_var["any_opa_" + code] = all_opa.where(
+            all_opa.treatment_function_code.is_in([code])
+            & all_opa.appointment_date.is_on_or_after("2022-06-01")
+        ).exists_for_patient()
+
+        dataset.add_column(f"any_pfu_{code}", count_var["any_pfu_" + code])
+        dataset.add_column(f"any_opa_{code}", count_var["any_opa_" + code])
+
+
     ###################################
 
     # outpatient visits before start of personalised followup
