cc0f89970f36f014dede5cbf93475594f8022d99
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2025-08-26 14:23:55 +0000

    measures by region

---
+++ analysis/process/measures_region_process.R
@@ -0,0 +1,72 @@
+
+# Import libraries #
+library('tidyverse')
+library('fs')
+
+# Create directory
+dir_create(here::here("output", "processed"), showWarnings = FALSE, recurse = TRUE)
+
+# Rounding and redaction
+rounding <- function(vars) {
+  case_when(vars == 0 ~ 0,
+            vars > 7 ~ round(vars / 5) * 5)
+}
+
+
+measures_region <- function(region) {
+  
+  measures <- read_csv(here::here("output", "measures", paste0("measures_", region, ".csv"))) %>%
+    subset(interval_start < as.Date("2025-06-01")) %>%
+    select(!"ratio") %>%
+    rename(month = interval_start) 
+  
+  measures_wide <- measures %>%
+    select(!("denominator")) %>%
+    spread("measure", "numerator")
+  
+  measure_total_denom <- measures %>% 
+    subset(measure == "count_opa") %>%
+    select(c("denominator", "month")) %>%
+    rename(total_pop = denominator)
+  
+  measure_opa_denom <- measures %>% 
+    subset(measure == "count_pfu") %>%
+    select(c("denominator", "month")) %>%
+    rename(opa_pop = denominator)
+  
+  all <- measures_wide %>% 
+    merge(measure_total_denom, all = T) %>%
+    merge(measure_opa_denom, all = T) %>%
+    replace(is.na(.), 0) %>%
+    mutate(across(c(starts_with(c("count","patients","total","any","opa"))), rounding),
+           region = region)
+  
+  overall <- all %>%
+    select(c("region", "month", "total_pop", "count_opa", "count_pfu", "patients_opa", 
+             "patients_pfu"))
+  
+  by_specialty <- all %>%
+    select(!c("count_opa", "count_pfu", "patients_opa", "patients_pfu", "total_pop", "opa_pop"))
+
+# Save
+write.csv(overall, file = here::here("output", "processed", paste0("ts_", region, ".csv")), row.names = FALSE)
+write.csv(by_specialty, file = here::here("output", "processed", paste0("ts_spec_", region, ".csv")), row.names = FALSE)
+
+}
+
+
+measures_region("london")
+measures_region("northeast")
+measures_region("northwest")
+measures_region("yorkshire")
+measures_region("eastmidlands")
+measures_region("westmidlands")
+measures_region("east")
+measures_region("southeast")
+measures_region("southwest")
+
+###########################################################
+
+
+
+  
