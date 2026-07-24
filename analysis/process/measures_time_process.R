01b78ce7df0062fae698ba43fda9a97c88ecf24b
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2026-07-24 11:19:17 +0000

    add missing package

--- analysis/process/measures_time_process.R
+++ analysis/process/measures_time_process.R
@@ -6,6 +6,7 @@ library(tidyr)
 library(here)
 library(glue)
 library(tidyverse)
+library(fs)
 
 # Create directory
 dir_create(here::here("output", "processed"), recurse = TRUE)
@@ -49,3 +50,4 @@ process_measures_time <- function(specialty) {
 process_measures_time("rheum")
 process_measures_time("gastro")
 process_measures_time("derm")
+
