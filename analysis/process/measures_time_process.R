837926f60a7816f5a2047b307183da85462b0994
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2026-08-10 14:31:22 +0100

    Add description for outpatient_time files
    Added detailed description for outpatient_time files, explaining the data captured and its relevance to PIFU pathways in rheumatology, gastroenterology, and dermatology.

--- analysis/process/measures_time_process.R
+++ analysis/process/measures_time_process.R
@@ -44,3 +44,18 @@ process_measures_time("rheum")
 process_measures_time("gastro")
 process_measures_time("derm")
 
+
+# Description of outpatient_time* files 
+
+#These files capture the number of outpatient attendances per 100 patients over 4-week periods relative to when patients 
+#were placed on PIFU (patient-initiated follow-up). This was done for rheumatology, gastroenterology, and dermatology.
+#The study population is all people who had a recorded flag for having been placed on a PIFU pathway for each
+#specialty. 
+
+#- n_patients: Number of patients with a PIFU flag who were still alive and registered in that 4-week period;
+#- n_attendances: Number of outpatient attendances in that 4-week period;
+#- time: Time period;
+#- period: Whether the time period is pre- or post- being placed on a PIFU pathway;
+#- type: Whether the row is for people MOVED (4) vs DISCHARGED (5) to a PIFU pathway, or all combined (NA);
+#- group: Whether the row is group by type ("PFU type"), or all combined ("All PFU");
+#- specialist: Whether the attendances included are ALL outpatient attendances, or just those for the given specialty (rheumatology, gastro, dermatology).
